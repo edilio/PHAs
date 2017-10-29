e359e2aecdd6088f3ff2f08b316083d7987a7ec4
Edilio Gallardo <edilio@degconnect.com>
2017-10-29 19:42:11 -0400

    dotenv for static

--- src/pha_locator/pha_locator/settings.py
+++ src/pha_locator/pha_locator/settings.py
@@ -11,10 +11,14 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# pull env variables from .env
+env_file = os.path.join(BASE_DIR, '.env')
+dotenv.read_dotenv(env_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -123,6 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATIC_ROOT = os.environ.get('STATIC_ROOT') or os.path.join(BASE_DIR, 'static/')
 
 
 REST_FRAMEWORK = {
